81c3710bc987797c4906ed3f320f820e7024a853
linliu0624 <39767252+linliu0624@users.noreply.github.com>
2019-07-19 15:26:33 +0900

    Update language.cpp

--- Dungeon/language.cpp
+++ Dungeon/language.cpp
@@ -1,12 +1,13 @@
-﻿#include "language.h"
+﻿#include "pch.h"
+#include "language.h"
 using namespace std;
 
 vector<string> vecStrEnglish;
-vector<string> vecStrJapaness;
+vector<string> vecStrJapaness = { "aaa","我" };
 
 
 
 void ChooseLnaguage(int type)
 {
-
+	cout << vecStrJapaness[1] << endl;
 }
